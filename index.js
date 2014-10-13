61b9f142765ceb53cd9e7101bd708948fd9f6180
John Heroy <johnheroy@gmail.com>
2014-10-13 15:56:25 -0700

    Handle 5th or neutral tone

--- index.js
+++ index.js
@@ -28,30 +28,34 @@ var prettify = function(str){
     var syllable = syllables[i];
     var tone = parseInt(syllable[syllable.length-1]);
     
-    if (tone <= 0 || tone > 4) console.error('invalid tone number:', tone, 'in', syllable);
+    if (tone <= 0 || tone > 5) {
+      console.error('invalid tone number:', tone, 'in', syllable);
+    } else if (tone === 5){
+      syllables[i] = syllable.slice(0, syllable.length - 1);
+    } else {
+      for (var j = 0; j < syllable.length; j++){
+        var currentLetter = syllable[j];
+        var nextLetter = syllable[j + 1];
 
-    for (var j = 0; j < syllable.length; j++){
-      var currentLetter = syllable[j];
-      var nextLetter = syllable[j + 1];
+        // found a vowel
+        if (replacements[currentLetter]){
+          var replaced;
+          var letterToReplace;
 
-      // found a vowel
-      if (replacements[currentLetter]){
-        var replaced;
-        var letterToReplace;
+          // two consecutive vowels
+          if (replacements[nextLetter] && medials.indexOf(currentLetter) >= 0){
+            letterToReplace = nextLetter;
+          } else {
+            letterToReplace = currentLetter;
+          }
 
-        // two consecutive vowels
-        if (replacements[nextLetter] && medials.indexOf(currentLetter) >= 0){
-          letterToReplace = nextLetter;
-        } else {
-          letterToReplace = currentLetter;
+          replaced = syllable.replace(letterToReplace, replacements[letterToReplace][tone - 1]);
+          syllables[i] = replaced.slice(0, replaced.length - 1);
+          break;
         }
+      }  
+    }
 
-        replaced = syllable.replace(letterToReplace, replacements[letterToReplace][tone - 1]);
-        syllables[i] = replaced.slice(0, replaced.length - 1);
-        break;
-      }
-
-    }  
   }
   return syllables.join(' ');
 };
